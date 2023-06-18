b3cd29c6ca5a85a25710abd1c57ccbbe839fad66
shinmyou <shinntaku2345@gmail.com>
2023-06-18 11:32:54 +0900

    🎉 section04-css

---
+++ section4-css/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>Hello2</h2>
+      <br />
+    </div>
+  );
+}
+
+export default App;
