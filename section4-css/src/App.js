a299026c694b697d97b93ce0596b2453be7f118d
shinmyou <shinntaku2345@gmail.com>
2023-06-18 11:49:32 +0900

    ✨ InlineStyle CSS

--- section4-css/src/App.js
+++ section4-css/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import InlineStyle from "./components/InlineStyle";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>Hello2</h2>
-      <br />
+      <InlineStyle />
     </div>
   );
 }
