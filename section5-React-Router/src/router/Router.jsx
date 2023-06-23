2a30cdb2e36302b10e7aefca2216230773f603a7
shinmyou <shinntaku2345@gmail.com>
2023-06-23 23:58:30 +0900

    ✨ ルート定義の分割

---
+++ section5-React-Router/src/router/Router.jsx
@@ -0,0 +1,35 @@
+import { Switch, Route } from "react-router-dom";
+import Home from "../Home";
+import Page2 from "../Page2";
+import { Page1Routes } from "./Page1Routes";
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="/page2">
+        <Page2 />
+      </Route>
+    </Switch>
+  );
+};
+
+export default Router;
