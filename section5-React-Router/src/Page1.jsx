1fa752e788c2188fee0de6cd7034a27a418cc73e
shinmyou <shinntaku2345@gmail.com>
2023-06-25 17:21:36 +0900

    ✨ stateを渡すページ遷移

--- section5-React-Router/src/Page1.jsx
+++ section5-React-Router/src/Page1.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 const Page1 = () => {
+  const arr = [...Array(10).keys()];
+
   return (
     <div>
       <h1>Page1ページです。</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
     </div>
--- section5-React-Router/src/Page1DetailA.jsx
+++ section5-React-Router/src/Page1DetailA.jsx
@@ -1,4 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 const Page1DetailA = () => {
+  // const location = useLocation();
+  // console.log(location);
+  const { state } = useLocation();
+  console.log(state); // 結果：[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   return (
     <div>
       <h1>Page1DetailAページです。</h1>
