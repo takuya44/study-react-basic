57c7b68a31718c3d6991d3d5ef3d398a0e2bb8bd
shinmyou <shinntaku2345@gmail.com>
2023-06-25 17:01:32 +0900

    ✨ クエリパラメータを扱う

--- section5-React-Router/src/UrlParameter.jsx
+++ section5-React-Router/src/UrlParameter.jsx
@@ -1,12 +1,20 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  useLocation,
+  useParams,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
+  // console.log(query.get("name"));
 
   return (
     <div>
       <h1>UrlParameterページです。</h1>
       <p>パラメーターは{id}です</p>
+      <p>パラメーターは{query.get("name")}です</p>
     </div>
   );
 };
