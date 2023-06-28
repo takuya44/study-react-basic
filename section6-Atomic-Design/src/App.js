27e054040f711485ae69c059d28edb620eff519f
shinmyou <shinntaku2345@gmail.com>
2023-06-29 06:58:30 +0900

    ✨ Organisms（UserCard）

--- section6-Atomic-Design/src/App.js
+++ section6-Atomic-Design/src/App.js
@@ -2,6 +2,18 @@ import "./App.css";
 import PrimaryButton from "./components/atoms/button/PrimaryButton";
 import SecondaryButton from "./components/atoms/button/SecondaryButton";
 import SearchInput from "./components/molecules/SearchInput";
+import UserCard from "./components/organisms/user/UserCard";
+
+const user = {
+  name: "シモン",
+  image: "./img/dog01.jpg",
+  email: "1111@aaa.com",
+  phone: "090-1111-1111",
+  company: {
+    name: "テスト株式会社",
+  },
+  website: "https://google.com",
+};
 
 function App() {
   return (
@@ -10,6 +22,7 @@ function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
