f1bf13310d4ed0bd3263ecbf2afb90df6bfebd26
shinmyou <shinntaku2345@gmail.com>
2023-07-01 23:36:45 +0900

    ✨ ツラい例を実装

--- section7-State-Kanri/src/components/organisms/user/UserCard.jsx
+++ section7-State-Kanri/src/components/organisms/user/UserCard.jsx
@@ -3,11 +3,11 @@ import Card from "../../atoms/card/Card";
 import UserIconWithName from "../../molecules/user/UserIconWithName";
 
 const UserCard = (props) => {
-  const { user } = props;
+  const { user, isAdmin } = props;
 
   return (
     <Card>
-      <UserIconWithName name={user.name} image={user.image} />
+      <UserIconWithName name={user.name} image={user.image} isAdmin={isAdmin} />
       <SDl>
         <dt>メール</dt>
         <dd>{user.email}</dd>
--- section7-State-Kanri/src/components/pages/Top.jsx
+++ section7-State-Kanri/src/components/pages/Top.jsx
@@ -1,9 +1,24 @@
+import React from "react";
 import styled from "styled-components";
+import SecondaryButton from "../atoms/button/SecondaryButton";
+import { useHistory } from "react-router-dom";
 
 const Top = () => {
+  const history = useHistory();
+
+  const onClickAdmin = () =>
+    history.push({ pathname: "/users", state: { isAdmin: true } });
+
+  const onClickGeneral = () =>
+    history.push({ pathname: "/users", state: { isAdmin: false } });
+
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
@@ -12,4 +27,5 @@ export default Top;
 
 const SContainer = styled.div`
   text-align: center;
+  height: 100vh;
 `;
