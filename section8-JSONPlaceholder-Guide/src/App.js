761c8c1918279d74faec4d7227ed031f3cfdeee3
shinmyou <shinntaku2345@gmail.com>
2023-07-02 22:42:29 +0900

    🎉 JSONPlaceholder

---
+++ section8-JSONPlaceholder-Guide/src/App.js
@@ -0,0 +1,30 @@
+import { useCallback, useMemo, useState } from "react";
+import ChildArea from "./ChildArea";
+import "./App.css";
+
+function App() {
+  console.log("親コンポーネントがレンダリングされました");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
+
+export default App;
