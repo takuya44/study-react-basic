65b330b7ddec63e9d54c8bc08737d00b349324ee
shinmyou <shinntaku2345@gmail.com>
2023-07-03 22:57:40 +0900

    🎉 section9-reactts

---
+++ section9-reactts/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi there!</h1>
+      <h2>Hi there!Hi there!</h2>
+    </div>
+  );
+}
+
+export default App;
