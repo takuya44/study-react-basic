e7a96495c3e0a89320369dd0ac108d4d48d3ce94
shinmyou <shinntaku2345@gmail.com>
2023-07-05 23:20:03 +0900

    ✨ 型定義を効率的に管理

--- section10-reactts/src/App.tsx
+++ section10-reactts/src/App.tsx
@@ -2,13 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 import Todo from "./Todo";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
 
 function App() {
   const [todos, setTodos] = useState<TodoType[]>([]);
@@ -27,7 +21,7 @@ function App() {
       <button onClick={onClickFetchData}>Click me</button>
       {todos.map((todo: any) => (
         // <Todo todo={todo} />
-        <Todo title={todo.title} userId={todo.userId} />
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} />
       ))}
     </div>
   );
--- section10-reactts/src/Todo.tsx
+++ section10-reactts/src/Todo.tsx
@@ -1,10 +1,10 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-const Todo = (props: TodoType) => {
+// 型指定①
+// Pick：(props: Pick<TodoType, "title" | "userId" | "completed">)
+// 型指定② 今回はidのみ不要なので、Omitでidを除外する。こちらの方が楽。
+//  Omit<TodoType, "id">
+const Todo = (props: Omit<TodoType, "id">) => {
   //   const { todo } = props;
   //   const { title, userId } = todo;
   const { title, userId, completed = false } = props;
