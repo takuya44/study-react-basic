d8fb28fc07c8a5b32fa29b8cddf737a741914e1d
shinmyou <shinntaku2345@gmail.com>
2023-07-12 07:26:49 +0900

    ✨Globalなスタイルを設定

--- section12-chakraUI/src/App.tsx
+++ section12-chakraUI/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Button, ButtonGroup, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <ChakraProvider>
-      <Button>ボタン</Button>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
     </ChakraProvider>
   );
 }
