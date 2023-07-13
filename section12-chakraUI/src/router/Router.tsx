1b768f24feabc5e37e1b50bab0ca59568bfe2cfd
shinmyou <shinntaku2345@gmail.com>
2023-07-13 23:58:21 +0900

    🚑

--- section12-chakraUI/src/router/Router.tsx
+++ section12-chakraUI/src/router/Router.tsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import { HomeRoutes } from "./HomeRoutes";
 import Login from "../components/pages/Login";
 import Page404 from "../components/pages/Page404";
+import HeaderLayout from "../components/templates/HeaderLayout";
 
 const Router: VFC = memo(() => {
   return (
@@ -21,7 +22,7 @@ const Router: VFC = memo(() => {
                 exact={route.exact}
                 path={`${url}${route.path}`}
               >
-                {route.children}
+                <HeaderLayout>{route.children}</HeaderLayout>
               </Route>
             ))}
           </Switch>
