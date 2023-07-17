5deda92dedc9641d3c2eb1bc01abce5728b2fd80
shinmyou <shinntaku2345@gmail.com>
2023-07-17 23:20:36 +0900

    ♻️ ユーザー一覧画面の作成（コンポーネント分割）

---
+++ section12-chakraUI/src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,44 @@
+import { memo, ReactNode, VFC } from "react";
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+
+// import dog01 from "../../../img/dog01.jpg";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full"
+          boxSize="160px"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
+
+export default UserCard;
--- section12-chakraUI/src/components/pages/UserManagement.tsx
+++ section12-chakraUI/src/components/pages/UserManagement.tsx
@@ -2,36 +2,17 @@ import { memo, VFC } from "react";
 import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 
 import dog01 from "../../img/dog01.jpg";
+import UserCard from "../organisms/user/UserCard";
 
 const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src={dog01}
-              alt="プロフィール画像"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              シモン
-            </Text>
-            <Text fontSize="sm" color="gray">
-              Shimon Takuya
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl={dog01}
+          userName="シモン"
+          fullName="Ahimonn takuya"
+        />
       </WrapItem>
     </Wrap>
   );
